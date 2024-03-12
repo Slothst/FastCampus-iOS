cf8ad77f370be50845ffad8e00aaaacf61de26df
Slothst <cnj991279@gmail.com>
2024-03-12 17:39:02 +0900

    스토리보드를 활용한 명상 앱 뷰 구성

---
+++ 06_02-HeadSpaceFocus/HeadSpaceFocus/FocusCell.swift
@@ -0,0 +1,12 @@
+//
+//  FocusCell.swift
+//  HeadSpaceFocus
+//
+//  Created by 최낙주 on 3/12/24.
+//
+
+import UIKit
+
+class FocusCell: UICollectionViewCell {
+    
+}
---
+++ 06_02-HeadSpaceFocus/HeadSpaceFocus/FocusViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by 최낙주 on 3/12/24.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
