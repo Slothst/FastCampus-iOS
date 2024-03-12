28460e2a8c0cee6575e30b2ecbedabfc6d161cb2
Slothst <cnj991279@gmail.com>
2024-03-12 18:14:32 +0900

    Diffable DataSource와 Compositional Layout을 활용한 명상 앱 구현

--- 06_02-HeadSpaceFocus/HeadSpaceFocus/FocusCell.swift
+++ 06_02-HeadSpaceFocus/HeadSpaceFocus/FocusCell.swift
@@ -9,4 +9,18 @@ import UIKit
 
 class FocusCell: UICollectionViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        contentView.backgroundColor = .systemIndigo
+        contentView.layer.cornerRadius = 10
+    }
+    
+    func configure(_ item: Focus) {
+        titleLabel.text = item.title
+        descriptionLabel.text = item.description
+        thumbnailImageView.image = UIImage(systemName: item.imageName)?.withRenderingMode(.alwaysOriginal)
+    }
 }
