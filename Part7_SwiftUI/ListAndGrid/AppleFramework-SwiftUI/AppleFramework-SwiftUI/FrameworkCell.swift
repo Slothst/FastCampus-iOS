53c34268caafe44a1a2b920df12657981c123996
Slothst <cnj991279@gmail.com>
2024-03-18 22:07:16 +0900

    SwiftUI AppleFramework 구현

---
+++ Part7_SwiftUI/ListAndGrid/AppleFramework-SwiftUI/AppleFramework-SwiftUI/FrameworkCell.swift
@@ -0,0 +1,30 @@
+//
+//  FrameworkCell.swift
+//  AppleFramework-SwiftUI
+//
+//  Created by 최낙주 on 3/18/24.
+//
+
+import SwiftUI
+
+struct FrameworkCell: View {
+    
+    var framework: AppleFramework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+            Text(framework.name)
+                .font(.system(size: 16, weight: .bold))
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    FrameworkCell(framework: AppleFramework.list[0])
+        .previewLayout(.fixed(width: 160, height: 250))
+}
