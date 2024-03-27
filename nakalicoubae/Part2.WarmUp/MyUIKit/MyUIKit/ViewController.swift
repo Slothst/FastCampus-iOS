830bdecfff06d85f871a9e6527995aeb3fbe3d44
Slothst <cnj991279@gmail.com>
2024-03-27 20:03:31 +0900

    Storyboard로 UIKit 입문하기

---
+++ nakalicoubae/Part2.WarmUp/MyUIKit/MyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUIKit
+//
+//  Created by 최낙주 on 3/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+
+
+}
+
