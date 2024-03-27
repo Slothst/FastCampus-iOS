968a41ef0387bdf252f33b4424bf26b78177103f
Slothst <cnj991279@gmail.com>
2024-03-27 19:19:42 +0900

    네비게이션 써보기

---
+++ nakalicoubae/Part2.WarmUp/WarmUp/WarmUp/03/Navigation.swift
@@ -0,0 +1,46 @@
+//
+//  Navigation.swift
+//  WarmUp
+//
+//  Created by 최낙주 on 3/27/24.
+//
+
+import SwiftUI
+
+struct Navigation: View {
+    
+    let titles = ["디테일뷰로 이동하기1", "디테일뷰로 이동하기2"]
+    let descriptions = ["데스티네이션입니다.", "데스티네이션입니다.2"]
+    
+    @State var showModal: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach([0, 1], id: \.self) { index in
+                    NavigationLink {
+                        Text(descriptions[index])
+                    } label: {
+                        Text(titles[index])
+                    }
+                }
+            }
+            .toolbar(content: {
+                Button {
+                    showModal = true
+                } label: {
+                    Text("add")
+                }
+
+            })
+            .sheet(isPresented: $showModal, content: {
+                Text("아이템 추가 페이지입니다.")
+            })
+            .navigationTitle("네비게이션")
+        }
+    }
+}
+
+#Preview {
+    Navigation()
+}
