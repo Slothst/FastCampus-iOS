3cf25b3ec58f9e1c86b40b626af235af17a37268
Slothst <cnj991279@gmail.com>
2024-03-27 14:06:20 +0900

    코딩 없이 화면 그리기

---
+++ nakalicoubae/Part2.WarmUp/WarmUp/WarmUp/WarmUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmUpApp.swift
+//  WarmUp
+//
+//  Created by 최낙주 on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
