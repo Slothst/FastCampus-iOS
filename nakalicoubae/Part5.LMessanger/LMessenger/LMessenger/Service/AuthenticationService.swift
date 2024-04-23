772054cb30ec2555daa2194878f6b28ae587386e
Slothst <cnj991279@gmail.com>
2024-04-24 00:18:48 +0900

    Firebase Auth 세팅 및 로그인 UI 구성하기

---
+++ nakalicoubae/Part5.LMessanger/LMessenger/LMessenger/Service/AuthenticationService.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationService.swift
+//  LMessenger
+//
+//  Created by 최낙주 on 4/23/24.
+//
+
+import Foundation
+
+protocol AuthenticationServiceType {
+    
+}
+
+class AuthenticationService: AuthenticationServiceType {
+    
+}
+
+class StubAuthenticationService: AuthenticationServiceType {
+    
+}
---
+++ nakalicoubae/Part5.LMessanger/LMessenger/LMessenger/View/MainTab/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  LMessenger
+//
+//  Created by 최낙주 on 4/23/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text("MainTabView")
+    }
+}
+
+#Preview {
+    MainTabView()
+}
