ab4e1c62df0b8d8ffedce885a3053cc346d1b12d
Slothst <cnj991279@gmail.com>
2024-04-24 20:48:05 +0900

    Google 로그인 구현하기

---
+++ nakalicoubae/Part5.LMessanger/LMessenger/LMessenger/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  LMessenger
+//
+//  Created by 최낙주 on 4/24/24.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var name: String
+    var phoneNumber: String
+    var profileURL: String?
+    var description: String?
+}
---
+++ nakalicoubae/Part5.LMessanger/LMessenger/LMessenger/Service/ServiceError.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceError.swift
+//  LMessenger
+//
+//  Created by 최낙주 on 4/24/24.
+//
+
+import Foundation
+
+enum ServiceError: Error {
+    case error(Error)
+}
