72b3269c0dcfc1e4293eec605551015b419806ff
Slothst <cnj991279@gmail.com>
2024-04-28 14:31:29 +0900

    이미지 캐시 구현하기

---
+++ nakalicoubae/Part5.LMessenger/LMessenger/LMessenger/Service/Upload/UploadService.swift
@@ -0,0 +1,32 @@
+//
+//  UploadService.swift
+//  LMessenger
+//
+//  Created by 최낙주 on 4/28/24.
+//
+
+import Foundation
+
+protocol UploadServiceType {
+    func uploadImage(source: UploadSourceType, data: Data) async throws -> URL
+}
+
+class UploadService: UploadServiceType {
+    
+    private let provider: UploadProviderType
+    
+    init(provider: UploadProviderType) {
+        self.provider = provider
+    }
+    
+    func uploadImage(source: UploadSourceType, data: Data) async throws -> URL {
+        let url = try await provider.upload(path: source.path, data: data, fileName: UUID().uuidString)
+        return url
+    }
+}
+
+class StubUploadService: UploadServiceType {
+    func uploadImage(source: UploadSourceType, data: Data) async throws -> URL {
+        return URL(string: "")!
+    }
+}
