8fe832b39c3425d6fa72fb83f07ef5367da14986
Slothst <cnj991279@gmail.com>
2024-05-19 18:36:50 +0900

    스플래시 화면 코드로 구현하기

---
+++ nakalicoubae/Part6.Cproject/Cproject/Cproject/SplashViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashViewController.swift
+//  Cproject
+//
+//  Created by 최낙주 on 5/19/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ nakalicoubae/Part6.Cproject/Cproject/Cproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cproject
+//
+//  Created by 최낙주 on 5/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
