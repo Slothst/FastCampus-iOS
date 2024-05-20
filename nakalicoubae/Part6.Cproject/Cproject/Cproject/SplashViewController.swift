6807db7b11815fd41143a469909b177a0995a7e9
Slothst <cnj991279@gmail.com>
2024-05-20 19:11:50 +0900

    Autolayout을 이용하여 애니메이션 구현

--- nakalicoubae/Part6.Cproject/Cproject/Cproject/SplashViewController.swift
+++ nakalicoubae/Part6.Cproject/Cproject/Cproject/SplashViewController.swift
@@ -9,7 +9,27 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    @IBOutlet weak var appIconCenterXConstaint: NSLayoutConstraint!
+    @IBOutlet weak var appIconCenterYConstaint: NSLayoutConstraint!
+    @IBOutlet weak var appIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        /*appIconCenterXConstaint.constant = -(view.frame.width / 2) - (appIcon.frame.width / 2)
+        appIconCenterYConstaint.constant = -(view.frame.height / 2) - (appIcon.frame.height / 2)
+        
+        UIView.animate(withDuration: 1) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }*/
+        
+        UIView.animate(withDuration: 3) { [weak self] in
+            let rotationAngle: CGFloat = CGFloat.pi
+            self?.appIcon.transform = CGAffineTransform(rotationAngle: rotationAngle)
+        }
     }
 }
